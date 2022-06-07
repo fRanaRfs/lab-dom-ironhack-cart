65f5225f2ce3f19e996aadc22622ff106bbb59d0
fRanaRfs <franarfs.bureau@gmail.com>
2022-06-07 21:47:59 +0200

    DOM Lab
    Push

--- js/index.js
+++ js/index.js
@@ -2,16 +2,45 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  let priceElement = product.querySelector('.price span');
+  let price = Number (priceElement.innerHTML);
 
-  //... your code goes here
+  let quantityElement = product.querySelector('.quantity input')
+  let quantity = Number (quantityElement.value)
+  let subtotal = price * quantity
+  let subtotalElement = product.querySelector('.subtotal span')
+
+  subtotalElement.innerHTML = subtotal
+  return subtotal
+
 }
 
-function calculateAll() {
+function calculateAll()  {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelectorAll('.product');
+  //updateSubtotal(singleProduct);
   // end of test
+
+
+
+//// For each para recorrer updateSubtotal ////
+ let allItems = document.querySelectorAll('.product');
+ allItems.forEach((item)=>{updateSubtotal(item)});
+//////////////////////////////////////////////
+let productsElements = document.querySelectorAll('.product');
+let total = updateSubtotal(productsElements[1]);
+let totalElement = document.querySelector('#total-value span');
+totalElement.innerText = total;
+
+
+
+
+//let totalValueElement = document.querySelector('#total-value span');
+//let totalValue = Number (totalValueElement.value);
+//let total = totalValue + totalValueElement.innerText;
+//return total;
+
 
   // ITERATION 2
   //... your code goes here
